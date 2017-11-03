1be1bf858c25ef6cef3c52dadd2e1f1a34593597
garickjay <garickjay@sina.com>
2017-11-03 20:02:06 +0800

    调整滑动条

--- public/js/plus.js
+++ public/js/plus.js
@@ -1,31 +1,31 @@
 /**
  * Created by lenovo on 2017/9/5.
  */
-// $(document).ready(function(){
-//     $(".tpl-content-wrapper").height($(window).height()-150);
+$(document).ready(function(){
+    $(".tpl-content-wrapper").height($(window).height()-150);
 
-//     $("#main-iframe").height($(window).height()-110);
+    $("#main-iframe").height($(window).height()-110);
 
-//     $(".tpl-left-nav").height($(window).height()-150);
-//     if($(window).width()<1000){
-//         $(".tpl-content-wrapper").width($(window).width());
-//         $("#main-iframe").width($(window).width());
-//     }
-//     var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
-//     if (userAgent.indexOf("Chrome") > -1){
-//         $(".tpl-left-nav").height($(document.body).height()-45);
-//         $(".b-box").height($(document.body).height()-95);
-//         $(".tpl-content-wrapper").width($(window).width()-210);
-//         $("#main-iframe").width($(window).width()-210);
-//     }else{
-//         $(".tpl-content-wrapper").width($(window).width()-230);
-//         $("#main-iframe").width($(window).width()-230);
-//         $(".tpl-left-nav").height($(document.body).height()+45);
-//         $(".b-box").height($(document.body).height()+45);
-//     }
-//     $(".tpl-left-nav-item a").click(function(){
-//         $(".tpl-left-nav-item a").removeClass("active");
-//         $(this).addClass("active");
-//     });
-// });
+    $(".tpl-left-nav").height($(window).height()-150);
+    if($(window).width()<1000){
+        $(".tpl-content-wrapper").width($(window).width());
+        $("#main-iframe").width($(window).width());
+    }
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+    if (userAgent.indexOf("Chrome") > -1){
+        $(".tpl-left-nav").height($(document.body).height()-45);
+        $(".b-box").height($(document.body).height()-95);
+        $(".tpl-content-wrapper").width($(window).width()-240);
+        $("#main-iframe").width($(window).width()-240);
+    }else{
+        $(".tpl-content-wrapper").width($(window).width()-245);
+        $("#main-iframe").width($(window).width()-245);
+        $(".tpl-left-nav").height($(document.body).height()+45);
+        $(".b-box").height($(document.body).height()+45);
+    }
+    $(".tpl-left-nav-item a").click(function(){
+        $(".tpl-left-nav-item a").removeClass("active");
+        $(this).addClass("active");
+    });
+});
 
