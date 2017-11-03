9a17e0f90f790130b0b38ce849b50a4cb5d517e4
Freak <freak_ldx@163.com>
2017-11-03 22:10:00 +0800

    修改滑动条

--- public/js/plus.js
+++ public/js/plus.js
@@ -7,22 +7,36 @@ $(document).ready(function(){
     $("#main-iframe").height($(window).height()-110);
 
     $(".tpl-left-nav").height($(window).height()-150);
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
     if($(window).width()<1000){
         $(".tpl-content-wrapper").width($(window).width());
         $("#main-iframe").width($(window).width());
-    }
-    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
-    if (userAgent.indexOf("Chrome") > -1){
-        $(".tpl-left-nav").height($(document.body).height()+45);
-        $(".b-box").height($(document.body).height()+45);
-        $(".tpl-content-wrapper").width($(window).width()-240);
-        $("#main-iframe").width($(window).width()-240);
-    }else{
-        $(".tpl-content-wrapper").width($(window).width()-245);
-        $("#main-iframe").width($(window).width()-245);
-        $(".tpl-left-nav").height($(document.body).height()+45);
-        $(".b-box").height($(document.body).height()+45);
-    }
+    }else if(1000<$(window).width()<1280){
+		
+		if (userAgent.indexOf("Chrome") > -1){
+		    $(".tpl-left-nav").height($(document.body).height()+45);
+		    $(".b-box").height($(document.body).height()+45);
+		    $(".tpl-content-wrapper").width($(window).width()-240);
+		    $("#main-iframe").width($(window).width()-240);
+		}else{
+		    $(".tpl-content-wrapper").width($(window).width()-245);
+		    $("#main-iframe").width($(window).width()-245);
+		    $(".tpl-left-nav").height($(document.body).height()+45);
+		    $(".b-box").height($(document.body).height()+45);
+		}
+	}else{
+		if (userAgent.indexOf("Chrome") > -1){
+		    $(".tpl-left-nav").height($(document.body).height()+45);
+		    $(".b-box").height($(document.body).height()+45);
+		    $(".tpl-content-wrapper").width($(window).width()-240);
+		    $("#main-iframe").width($(window).width()-240);
+		}else{
+		    $(".tpl-content-wrapper").width($(window).width()-245);
+		    $("#main-iframe").width($(window).width()-245);
+		    $(".tpl-left-nav").height($(document.body).height()+45);
+		    $(".b-box").height($(document.body).height()+45);
+		}
+	}
     $(".tpl-left-nav-item a").click(function(){
         $(".tpl-left-nav-item a").removeClass("active");
         $(this).addClass("active");
