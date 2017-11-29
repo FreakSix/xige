d3a55c7dd29ac54e79e959481d42f12e58b5de7e
Freak <freak_ldx@163.com>
2017-11-29 16:40:18 +0800

    响应式修改

--- public/js/plus.js
+++ public/js/plus.js
@@ -2,43 +2,90 @@
  * Created by lenovo on 2017/9/5.
  */
 $(document).ready(function(){
-	
-
-    // $(".tpl-left-nav").height($(window).height()-150);
-    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+	var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
     if($(window).width()<1000){
         $(".tpl-content-wrapper").width($(window).width());
-    }else if($(window).width()>1390){
+    }else if($(window).width()>1400){
 		
 		if (userAgent.indexOf("Chrome") > -1){
-		    $(".tpl-left-nav").height($(document.body).height()-45);
-		    // $(".tpl-content-wrapper").width($(window).width()-240);
+		    $(".tpl-content-wrapper").width($(window).width()-230);
+		    $(".tpl-content-wrapper").height($(window).height()-70);
+		    $(".tpl-left-nav").height($(document.body).height());
+		    $(".tpl-left-nav-list-son").height($(window).height()-130);
+		}else if(userAgent.indexOf("Trident") > -1){
+		    $(".tpl-content-wrapper").width($(window).width()-230);
+		    $(".tpl-content-wrapper").height($(window).height()-70);
+			$(".tpl-left-nav").height($(window).height()-60);
+			$(".tpl-left-nav-list-son").height($(window).height()-130);
+		    
 		}else{
-		    // $(".tpl-content-wrapper").width($(window).width()-245);
-		    $(".tpl-left-nav").height($(document.body).height()+45);
+			$(".tpl-content-wrapper").width($(window).width()-230);
+		    $(".tpl-content-wrapper").height($(window).height()-70);
+		    $(".tpl-left-nav").height($(window).height()-60);
+		    $(".tpl-left-nav-list-son").height($(window).height()-120);
 		}
 	}else{
 		if (userAgent.indexOf("Chrome") > -1){
 		    $(".tpl-content-wrapper").width($(window).width()-230);
 		    $(".tpl-content-wrapper").height($(window).height()-70);
-		    // $(".tpl-portlet-components").width($(window).width()-230);
-		    // $(".tpl-portlet-components").height($(window).height()-70);
 		    $(".tpl-left-nav").height($(window).height()-60);
 		}else if(userAgent.indexOf("Trident") > -1){
 		    $(".tpl-content-wrapper").width($(window).width()-230);
 		    $(".tpl-content-wrapper").height($(window).height()-70);
-		    // $(".tpl-portlet-components").width($(window).width()-230);
-		    // $(".tpl-portlet-components").height($(window).height()-70);
 			$(".tpl-left-nav").height($(window).height()-60);
+			$(".tpl-left-nav-list-son").height($(window).height()-130);
 		}else{
 		    $(".tpl-content-wrapper").width($(window).width()-230);
-		    $(".tpl-content-wrapper").height($(window).height()-50);
-		    // $(".tpl-portlet-components").width($(window).width()-230);
-		    // $(".tpl-portlet-components").height($(window).height()-50);
+		    $(".tpl-content-wrapper").height($(window).height()-70);
 		    $(".tpl-left-nav").height($(window).height()-60);
 		    $(".tpl-left-nav-list-son").height($(window).height()-120);
 		}
 	}
+	
+	window.onresize = function(){
+	    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+	    if($(window).width()<1000){
+	        $(".tpl-content-wrapper").width($(window).width());
+	    }else if($(window).width()>1400){
+			
+			if (userAgent.indexOf("Chrome") > -1){
+			    $(".tpl-content-wrapper").width($(window).width()-230);
+			    $(".tpl-content-wrapper").height($(window).height()-70);
+			    $(".tpl-left-nav").height($(document.body).height());
+			    $(".tpl-left-nav-list-son").height($(window).height()-130);
+			}else if(userAgent.indexOf("Trident") > -1){
+			    $(".tpl-content-wrapper").width($(window).width()-230);
+			    $(".tpl-content-wrapper").height($(window).height()-70);
+				$(".tpl-left-nav").height($(window).height()-60);
+				$(".tpl-left-nav-list-son").height($(window).height()-130);
+			    
+			}else{
+				$(".tpl-content-wrapper").width($(window).width()-230);
+			    $(".tpl-content-wrapper").height($(window).height()-70);
+			    $(".tpl-left-nav").height($(window).height()-60);
+			    $(".tpl-left-nav-list-son").height($(window).height()-120);
+			}
+		}else{
+			if (userAgent.indexOf("Chrome") > -1){
+			    $(".tpl-content-wrapper").width($(window).width()-230);
+			    $(".tpl-content-wrapper").height($(window).height()-70);
+			    $(".tpl-left-nav").height($(window).height()-60);
+			}else if(userAgent.indexOf("Trident") > -1){
+			    $(".tpl-content-wrapper").width($(window).width()-230);
+			    $(".tpl-content-wrapper").height($(window).height()-70);
+				$(".tpl-left-nav").height($(window).height()-60);
+				$(".tpl-left-nav-list-son").height($(window).height()-130);
+			}else{
+			    $(".tpl-content-wrapper").width($(window).width()-230);
+			    $(".tpl-content-wrapper").height($(window).height()-70);
+			    $(".tpl-left-nav").height($(window).height()-60);
+			    $(".tpl-left-nav-list-son").height($(window).height()-120);
+			}
+		}
+	}
+
+
+
  //    $(".tpl-content-wrapper").height($(window).height()-150);
 
  //    $("#main-iframe").height($(window).height()-110);
