c560556876748eadc790a67a2a7215e6801076f6
Freak <freak_ldx@163.com>
2017-12-10 07:42:39 +0800

    个人中心页面修改

--- application/Home/Controller/PersonalCenterController.class.php
+++ application/Home/Controller/PersonalCenterController.class.php
@@ -14,5 +14,15 @@
 		public function updateUserInfo(){
 			$this->display("update_user_info");
 		}
+
+		// 修改密码页面
+		public function updateUserPwd(){
+			$this->display("update_password");
+		}
+
+		// 登录日志页面
+		public function loginLog(){
+			$this->display("login_log");
+		}
 		
 	}
